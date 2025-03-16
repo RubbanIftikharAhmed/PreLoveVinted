5ad57cc3c58975df3a114b6783f68e6ff42de83f
RubbanIftikhar <iamrubban@gmail.com>
2025-03-16 22:47:29 +0500

    Setting Up the Project

---
+++ VInted/VIntedTests/VIntedTests.swift
@@ -0,0 +1,17 @@
+//
+//  VIntedTests.swift
+//  VIntedTests
+//
+//  Created by Rubban Iftikhar on 16/03/2025.
+//
+
+import Testing
+@testable import VInted
+
+struct VIntedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
